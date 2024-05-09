e45d4ebc317e90bcfac708cc14338d9446cd3e73
Sezgin Ünal <sezgin.unal@zaxe.com>
2024-05-09 16:11:00 +0300

    Implement zaxe bed leveling option

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -3636,6 +3636,13 @@ void PrintConfigDef::init_fff_params()
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionBool(false));
 
+    def          = this->add("zaxe_bed_leveling", coBool);
+    def->label   = L("Mesh level (G29)");
+    def->tooltip = L("This flag enables/disables the mesh leveling after homing. If unchecked will use the previous bed leveling mesh. "
+                     "This can greatly reduce initial time to start the file.");
+    def->mode    = comAdvanced;
+    def->set_default_value(new ConfigOptionBool(false));
+
     def = this->add("seam_position", coEnum);
     def->label = L("Seam position");
     def->category = L("Quality");
--- src/libslic3r/PrintConfig.hpp
+++ src/libslic3r/PrintConfig.hpp
@@ -1129,6 +1129,9 @@ PRINT_CONFIG_CLASS_DEFINE(
     ((ConfigOptionStrings,              small_area_infill_flow_compensation_model))
 
     ((ConfigOptionBool,                has_scarf_joint_seam))
+
+    // Zaxe
+    ((ConfigOptionBool,                zaxe_bed_leveling))
 )
 
 // This object is mapped to Perl as Slic3r::Config::Print.
