13cdb3f6926daca44ae80680c3dc5e1236b4fc5e
Sezgin Ünal <sezgin.unal@zaxe.com>
2024-06-24 11:28:32 +0300

    Remove custom ip codes

--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -91,7 +91,6 @@
 #include "ParamsDialog.hpp"
 #include "KBShortcutsDialog.hpp"
 #include "DownloadProgressDialog.hpp"
-#include "CustomIPDialog.hpp"
 
 #include "BitmapCache.hpp"
 #include "Notebook.hpp"
