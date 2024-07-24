e74474421963923a91e25ca106ab9d7126cac4de
Sezgin Ünal <sezgin.unal@zaxe.com>
2024-07-24 14:42:20 +0300

    Fix zstd dependency issue

--- deps/Boost/Boost.cmake
+++ deps/Boost/Boost.cmake
@@ -15,11 +15,12 @@ orcaslicer_add_cmake_project(Boost
     URL_HASH SHA256=4d27e9efed0f6f152dc28db6430b9d3dfb40c0345da7342eaa5a987dde57bd95
     LIST_SEPARATOR |
     CMAKE_ARGS
-        -DBOOST_EXCLUDE_LIBRARIES:STRING=contract|fiber|numpy|stacktrace|wave|test
+        -DBOOST_EXCLUDE_LIBRARIES:STRING=contract|fiber|numpy|stacktrace|wave|test|zstd
         -DBOOST_LOCALE_ENABLE_ICU:BOOL=OFF # do not link to libicu, breaks compatibility between distros
         -DBUILD_TESTING:BOOL=OFF
         "${_context_abi_line}"
         "${_context_arch_line}"
+        -DBOOST_IOSTREAMS_ENABLE_ZSTD=OFF
 )
 
 set(DEP_Boost_DEPENDS ZLIB)
