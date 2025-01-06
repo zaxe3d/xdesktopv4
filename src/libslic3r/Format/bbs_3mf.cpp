c6ae58eb1519b3733a43406af64585371b51c3cc
Sezgin Ünal <sezgin.unal@zaxe.com>
2025-01-06 11:19:40 +0300

    3mf: Fix handle metadata

--- src/libslic3r/Format/bbs_3mf.cpp
+++ src/libslic3r/Format/bbs_3mf.cpp
@@ -3727,7 +3727,7 @@ void PlateData::parse_filament_info(GCodeProcessorResult *result)
                 m_is_bbl_3mf = true;
                 m_bambuslicer_generator_version = Semver::parse(m_curr_characters.substr(12));
             }
-            else if (boost::starts_with(m_curr_characters, "XDesktop-")) {
+            else if (boost::starts_with(m_curr_characters, "OrcaSlicer-")) {
                 m_is_bbl_3mf = true;
                 m_bambuslicer_generator_version = Semver::parse(m_curr_characters.substr(11));
             }
