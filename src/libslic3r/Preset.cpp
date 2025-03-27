27a6caa85696aa83ed4973213cd86805bf8c134f
Sezgin Ünal <sezgin.unal@zaxe.com>
2025-03-27 11:18:36 +0300

    config: Add new hidden config for using on material matching with printer

--- src/libslic3r/Preset.cpp
+++ src/libslic3r/Preset.cpp
@@ -850,7 +850,8 @@ static std::vector<std::string> s_Preset_filament_options {
     "filament_unloading_speed", "filament_unloading_speed_start", "filament_toolchange_delay", "filament_cooling_moves", "filament_stamping_loading_speed", "filament_stamping_distance",
     "filament_cooling_initial_speed", "filament_cooling_final_speed", "filament_ramming_parameters",
     "filament_multitool_ramming", "filament_multitool_ramming_volume", "filament_multitool_ramming_flow", "activate_chamber_temp_control",
-    "filament_long_retractions_when_cut","filament_retraction_distances_when_cut", "idle_temperature"
+    "filament_long_retractions_when_cut","filament_retraction_distances_when_cut", "idle_temperature",
+    "filament_zid"
     };
 
 static std::vector<std::string> s_Preset_machine_limits_options {
--- src/libslic3r/PrintConfig.hpp
+++ src/libslic3r/PrintConfig.hpp
@@ -1319,6 +1319,7 @@ PRINT_CONFIG_CLASS_DERIVED_DEFINE(
     ((ConfigOptionPoint,               bed_mesh_probe_distance))
     ((ConfigOptionFloat,               adaptive_bed_mesh_margin))
 
+    ((ConfigOptionStrings,              filament_zid))
 
 )
 
